6ca49e104de8c88446e0acef0912c2ff09bc0f01
oyeraghib <raghib.khan76@gmail.com>
2022-06-24 16:35:28 +0530

    add requestly sdk dependency

--- app/src/main/java/com/example/requestlyassignment/ui/MainActivity.kt
+++ app/src/main/java/com/example/requestlyassignment/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.requestlyassignment
+package com.example.requestlyassignment.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.requestlyassignment.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
